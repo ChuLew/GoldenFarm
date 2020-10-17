cf370bc7a4f3b7a2abebda84d5174c19e08dcefb
Mitch Lew <mitchginlew@gmail.com>
2020-10-16 20:41:17 -0400

    first commit

---
+++ src/test/java/com/monroe/louis/goldenfarm/GoldenFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monroe.louis.goldenfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
